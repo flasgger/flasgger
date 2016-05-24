9626e5f55e5aa94ae4ea9c46e849d14762d5c19c
Ivan Vergés <ivan@microstudi.net>
2016-05-24 12:32:15 +0200

    Update base.py

--- flasgger/base.py
+++ flasgger/base.py
@@ -215,6 +215,8 @@ class OutputView(MethodView):
             data['host'] = self.config.get('host')
         if self.config.get("basePath"):
             data["basePath"] = self.config.get('basePath')
+        if self.config.get("securityDefinitions"):
+            data["securityDefinitions"] = self.config.get('securityDefinitions')
 
         # set defaults from template
         if self.template is not None:
