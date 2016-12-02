flasgger/flasgger

commit eb69ce346e0425adfb51f35bcf80f6da9e4aff2e

Added support for dispatch request View (#34)
* added support for dispatch_request

* updated README docs for dispatch_request

* Fixed the indentation for README file

* added import statement in README

* refactored code wrt pep8 support

flasgger/base.py
@@ -179,6 +179,12 @@ class SpecsView(MethodView):
         )
 
 
+def is_valid_dispatch_view(endpoint):
+    klass = endpoint.__dict__.get('view_class', None)
+    return klass and hasattr(klass, 'dispatch_request') \
+        and hasattr(endpoint, 'methods')
+
+
 class OutputView(MethodView):
     def __init__(self, *args, **kwargs):
         view_args = kwargs.pop('view_args', {})
@@ -237,13 +243,20 @@ class OutputView(MethodView):
             endpoint = current_app.view_functions[rule.endpoint]
             methods = dict()
             for verb in rule.methods.difference(ignore_verbs):
-                if hasattr(endpoint, 'methods') and verb in endpoint.methods:
+                if is_valid_dispatch_view(endpoint):
+                    endpoint.methods = endpoint.methods or ['GET']
+                    if verb in endpoint.methods:
+                        methods[verb.lower()] = endpoint
+                elif hasattr(endpoint, 'methods') and verb in endpoint.methods:
                     verb = verb.lower()
                     methods[verb] = getattr(endpoint.view_class, verb)
                 else:
                     methods[verb.lower()] = endpoint
             operations = dict()
             for verb, method in methods.items():
+                klass = method.__dict__.get('view_class', None)
+                if klass and hasattr(klass, 'dispatch_request'):
+                    method = klass.__dict__.get('dispatch_request')
                 summary, description, swag = _parse_docstring(
                     method, self.process_doc, endpoint=rule.endpoint, verb=verb
                 )

flasgger/simple_test.py
@@ -10,7 +10,8 @@ pip install flasgger
 python simple_test.py
 """
 from flask import Flask, jsonify, request
-from flasgger import Swagger
+from base import Swagger
+from flask.views import View
 
 app = Flask(__name__)
 
@@ -45,4 +46,37 @@ def recs():
     size = int(request.args.get('size', 1))
     return jsonify({"result": "test" * size})
 
+
+class Foo(View):
+    methods = ['GET', 'POST']
+
+    def dispatch_request(self):
+        """
+        A simple test API
+        This endpoint does nothing
+        Only returns "test"
+        ---
+        tags:
+          - rohit
+        parameters:
+          - name: size
+            in: query
+            type: string
+            description: size of elements
+        responses:
+          200:
+            description: A single user item
+            schema:
+              id: return_test
+              properties:
+                result:
+                  type: string
+                  description: The test
+                  default: 'test'
+        """
+        size = int(request.args.get('size', 1))
+        return jsonify({"result": "test" * size})
+
+
+app.add_url_rule('/dispatch_request', view_func=Foo.as_view('foo'))
 app.run(debug=True)

commit 02f350fda4cac99afc08389a4f7baa3e9a7380e4

Fix the change that broke the method views
Commit eb69ce346e0425adfb51f35bcf80f6da9e4aff2e broke the case where the View is a method view. In this case we want to search for get and put methods and not the dispatch_request method

flasgger/base.py
@@ -183,6 +183,10 @@ def is_valid_dispatch_view(endpoint):
     klass = endpoint.__dict__.get('view_class', None)
     return klass and hasattr(klass, 'dispatch_request') \
         and hasattr(endpoint, 'methods')
+    
+def is_valid_method_view(endpoint):
+    klass = endpoint.__dict__.get('view_class', None)
+    return issubclass(klass, MethodView)
 
 
 class OutputView(MethodView):
@@ -242,8 +246,9 @@ class OutputView(MethodView):
         for rule in self.get_url_mappings(self.spec.get('rule_filter')):
             endpoint = current_app.view_functions[rule.endpoint]
             methods = dict()
+            is_method_view = is_valid_method_view(endpoint)
             for verb in rule.methods.difference(ignore_verbs):
-                if is_valid_dispatch_view(endpoint):
+                if not is_method_view and is_valid_dispatch_view(endpoint):
                     endpoint.methods = endpoint.methods or ['GET']
                     if verb in endpoint.methods:
                         methods[verb.lower()] = endpoint
@@ -255,7 +260,7 @@ class OutputView(MethodView):
             operations = dict()
             for verb, method in methods.items():
                 klass = method.__dict__.get('view_class', None)
-                if klass and hasattr(klass, 'dispatch_request'):
+                if not is_method_view and klass and hasattr(klass, 'dispatch_request'):
                     method = klass.__dict__.get('dispatch_request')
                 summary, description, swag = _parse_docstring(
                     method, self.process_doc, endpoint=rule.endpoint, verb=verb