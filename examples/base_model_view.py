51afb8a49fc86b8d57f496dab98d251e64f4715f
Bruno Rocha <rochacbruno@gmail.com>
2017-03-26 14:20:46 -0300

    Added base model view example

---
+++ examples/base_model_view.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify
+from flasgger import Swagger
+from flask.views import MethodView
+
+
+class BaseAPIView(MethodView):
+    """BAse view"""
+
+
+class ModelAPIView(BaseAPIView):
+    """Model api view"""
+
+
+class PostAPIView(ModelAPIView):
+
+    def get(self, team_id):
+        """
+        Get a list of users
+        First line is the summary
+        All following lines until the hyphens is added to description
+        ---
+        tags:
+          - users
+        parameters:
+          - name: team_id
+            in: path
+            description: ID of team (type any number)
+            required: true
+            type: int32
+        definitions:
+          User:
+            type: object
+            properties:
+              name:
+                type: string
+              team:
+                type: int32
+        responses:
+          200:
+            description: Returns a list of users
+            schema:
+              id: Users
+              type: object
+              properties:
+                users:
+                  type: array
+                  items:
+                    $ref: '#/definitions/User'
+            examples:
+              users: [{'name': 'Russel Allen', 'team': 66}]
+        """
+        data = {
+            "users": [
+                {"name": "Steven Wilson", "team": team_id},
+                {"name": "Mikael Akerfeldt", "team": team_id},
+                {"name": "Daniel Gildenlow", "team": team_id}
+            ]
+        }
+        return jsonify(data)
+
+
+app = Flask(__name__)
+Swagger(app)
+
+app.add_url_rule(
+    '/user/<team_id>',
+    view_func=PostAPIView.as_view('user'),
+    methods=['GET']
+)
+
+
+app.run(debug=True)
