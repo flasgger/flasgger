7aeab5ef01332b7186e65cc3f3a5ac3120cb4ef0
Bruno Rocha <rochacbruno@gmail.com>
2017-05-08 20:49:29 -0300

    Added examples and tests

---
+++ examples/colors_template_yaml.py
@@ -0,0 +1,39 @@
+"""
+Example loading all specs from YAML template
+"""
+from flask import Flask, jsonify
+
+from flasgger import Swagger
+
+app = Flask(__name__)
+app.config['SWAGGER'] = {
+    'title': 'Colors API',
+    'uiversion': 2
+}
+Swagger(app, template_file='colors_template.yaml')
+
+
+@app.route('/colors/<palette>/')
+def colors(palette):
+    """
+    Example using a dictionary as specification
+    This is the description
+    You can also set 'summary' and 'description' in
+    specs_dict
+    ---
+    # values here overrides the specs dict
+    deprecated: true
+    """
+    all_colors = {
+        'cmyk': ['cian', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
