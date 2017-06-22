9175081962c4cecd768c340fcc6a4dcd6433fa82
ro <allrod5@hotmail.com>
2017-06-22 11:27:46 -0300

    Code Cleanup

--- examples/disable_swagger_ui.py
+++ examples/disable_swagger_ui.py
@@ -16,29 +16,11 @@ swagger_config = {
             'model_filter': lambda tag: True,
         }
     ],
-    'swagger_ui': False,
-    'specs_route': ''
-}
-
-DEFAULT_CONFIG = {
-    "headers": [
-    ],
-    "specs": [
-        {
-            "endpoint": 'apispec_1',
-            "route": '/apispec_1.json',
-            "rule_filter": lambda rule: True,  # all in
-            "model_filter": lambda tag: True,  # all in
-        }
-    ],
-    "static_url_path": "/flasgger_static",
-    # "static_folder": "static",  # must be set by user
-    "swagger_ui": False,
-    "specs_route": "/apidoc/"
+    'swagger_ui': False
 }
 
 app = Flask(__name__)
-swag = Swagger(app, config=DEFAULT_CONFIG)
+swag = Swagger(app, config=swagger_config)
 
 
 def test_swag(client, specs_data):
