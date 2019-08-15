f7d0d93c9b7b05d195840bbe56fa0cab4c92bdd6
billyrrr <billrao@me.com>
2019-08-15 13:04:59 -0700

    Fix test cases for LazyDict and CachedLazyDict

--- unit_tests/test_utils.py
+++ unit_tests/test_utils.py
@@ -1,11 +1,13 @@
-from flasgger.utils import LazyDict
+from unittest.mock import call
+
+from flasgger.utils import LazyDict, CachedLazyDict
 import pytest
 from pytest_mock import mocker
 
 
 def test_lazy_dict(mocker):
 
-    stub = mocker.stub(name='on_something_stub')
+    stub = mocker.Mock(name='on_something_stub', return_value={"k": "v"})
 
     ld = LazyDict(stub)
 
@@ -14,4 +16,21 @@ def test_lazy_dict(mocker):
     d = ld._get_dict()
     d = ld._get_dict()
 
+    # Assert that func passed in LazyDict.__init__ is called twice
+    stub.assert_has_calls([call(), call()])
+
+
+def test_cached_dict(mocker):
+
+    stub = mocker.Mock(name='on_something_stub', return_value={"k": "v"})
+
+    ld = CachedLazyDict(stub)
+
+    stub.assert_not_called()
+
+    d = ld._get_dict()
+    d = ld._get_dict()
+
+    # Assert that func passed in LazyDict.__init__ is called once only
     stub.assert_called_once()
+
