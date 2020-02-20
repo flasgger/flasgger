d630b77ea53b960b0a9119af9f242d93b6e94272
Aleksandar <aleksandar.v90@gmail.com>
2020-02-20 14:27:05 +0100

    Enable custom oas3 fields
    (cherry picked from commit 083cd152941b27ae20f6fcbf1c467fc2dea95587)

--- flasgger/constants.py
+++ flasgger/constants.py
@@ -2,3 +2,7 @@ OPTIONAL_FIELDS = [
     'tags', 'consumes', 'produces', 'schemes', 'security',
     'deprecated', 'operationId', 'externalDocs'
 ]
+
+OPTIONAL_OAS3_FIELDS = [
+    'components', 'servers'
+]
