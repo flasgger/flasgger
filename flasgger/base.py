f5af35d1aa6f26f77bfc83ac130e0e72b0b650a5
Patrick Huck <phuck@lbl.gov>
2020-06-18 11:09:25 -0700

    fix pep8 tests

--- flasgger/base.py
+++ flasgger/base.py
@@ -247,11 +247,13 @@ class Swagger(object):
                 filename
             )
 
+        def yaml_safe_load(stream):
+            return yaml.safe_load(parse_imports(stream.read(), filename))
+
         if filename.endswith('.json'):
             loader = json.load
         elif filename.endswith('.yml') or filename.endswith('.yaml'):
-            loader = lambda stream: \
-                yaml.safe_load(parse_imports(stream.read(), filename))
+            loader = yaml_safe_load
         else:
             with codecs.open(filename, 'r', 'utf-8') as f:
                 contents = f.read()
@@ -259,8 +261,7 @@ class Swagger(object):
                 if contents[0] in ['{', '[']:
                     loader = json.load
                 else:
-                    loader = lambda stream: \
-                        yaml.safe_load(parse_imports(stream.read(), filename))
+                    loader = yaml_safe_load
         with codecs.open(filename, 'r', 'utf-8') as f:
             return loader(f)
 
