52fe4c3874153fa049a745d1d41147b7c2eb814c
billrao <billrao@me.com>
2023-06-10 14:20:51 -0700

    fix: pythonanywhere
    Signed-off-by: billrao <billrao@me.com>

--- scripts/setup_pythonanywhere.sh
+++ scripts/setup_pythonanywhere.sh
@@ -10,8 +10,11 @@ cd flasgger
 # Change to the desired branch if necessary
 # git checkout <branch_name>
 
-# Create a new virtual environment
-mkvirtualenv --python=python3.10
+## Create a new virtual environment
+#mkvirtualenv --python=python3.10
+
+virtualenv /home/flasgger/.virtualenvs/flasgger --python=python3.10
+source /home/flasgger/.virtualenvs/flasgger/bin/activate
 
 # Install the required dependencies
 pip3 install -r requirements.txt -r requirements-dev.txt
@@ -21,15 +24,4 @@ cd /var/www
 rm ${USERNAME}_pythonanywhere_com_wsgi.py
 ln -s /home/$USERNAME/flasgger/etc/pythonanywhere/pythonanywhere_com_wsgi.py ${USERNAME}_pythonanywhere_com_wsgi.py
 
-# Modify the web app configuration
-# Make sure to replace /home/<username> with your actual home directory path
-# Replace <username> with your PythonAnywhere username
-sed -i "s@<source_path>@/home/$USERNAME/flasgger@g" /var/www/${USERNAME}_pythonanywhere_com.conf
-sed -i "s@<working_directory>@/home/$USERNAME/flasgger@g" /var/www/${USERNAME}_pythonanywhere_com.conf
-sed -i "s@<virtualenv_path>@/home/$USERNAME/.virtualenvs/flasgger@g" /var/www/${USERNAME}_pythonanywhere_com.conf
-
-# Reload the web app
-# You will be prompted for your PythonAnywhere username and password
-pa_restart_webapp ${USERNAME}.pythonanywhere.com
-
-echo "Setup complete. Visit https://${USERNAME}.pythonanywhere.com/ to see the demo_app landing page."
+echo "Setup complete. Reload the web app and visit https://${USERNAME}.pythonanywhere.com/ to see the demo_app landing page."
